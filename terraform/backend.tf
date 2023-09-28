7a7231c28e40401b9f12e128dfa1a9641c9c4539
EvgeniyKoch <evgkoch@list.ru>
2023-09-28 14:37:32 +0300

    setup terraform

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "hex-tr-bucket"
-    key            = "terraform/state"
-    region         = "us-west-2"
-    dynamodb_table = "hex-tr-DynamoDB"
-    encrypt        = true
-  }
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,66 +1,20 @@
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Security group for web servers"
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  backend "s3" {
+    bucket         = "hex-tr-bucket"
+    key            = "terraform/state"
+    region         = "us-west-2"
+    dynamodb_table = "hex-tr-DynamoDB"
+    encrypt        = true
   }
 }
 
-resource "aws_instance" "web_servers" {
-  count         = 2
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
-
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  tags = {
-    Name = "WebServer-${count.index + 1}"
-  }
-}
-
-resource "aws_lb" "my_lb" {
-  name               = "my-lb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.web_sg.id]
-  subnets            = ["subnet-0c1c60c609838f5b1", "subnet-0332ed37372bda340", "subnet-0d9b3143117f842f7", "subnet-09bfe6229e45c7875"]
-
-  enable_deletion_protection = false
-}
-
-resource "aws_lb_listener" "client" {
-  load_balancer_arn = aws_lb.my_lb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.cert.arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.my_lb_tg.arn
-  }
-}
-
-resource "aws_lb_target_group" "my_lb_tg" {
-  name     = "my-lb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "vpc-05fddca804d444bc4"
-
-  health_check {
-    enabled = true
-    path    = "/"
-    port    = "80"
-  }
+provider "aws" {
+  region  = "us-west-2"
 }
