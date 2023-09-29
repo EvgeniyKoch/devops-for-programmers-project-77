2ed24e2e37a5c263297e4dadaad7d00498f9ec56
EvgeniyKoch <evgkoch@list.ru>
2023-09-29 13:13:47 +0300

    update ami and add ssh key

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "web_servers" {
   count         = 2
-  ami           = "ami-830c94e3"
+  ami           = "ami-05c9a95d305c1f5a3"
   instance_type = "t2.micro"
+  key_name      = "MyAWSKey"
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
